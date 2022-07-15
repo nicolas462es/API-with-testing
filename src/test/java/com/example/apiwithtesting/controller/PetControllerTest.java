f6bd51835e81690cc5a0f324bbae03e9d5f81454
nicolas462es <nicolas.espejo@kinandcarta.com>
2022-07-15 08:49:37 -0500

    findAllPets test

--- src/test/java/com/example/apiwithtesting/controller/PetControllerTest.java
+++ src/test/java/com/example/apiwithtesting/controller/PetControllerTest.java
@@ -2,10 +2,15 @@ package com.example.apiwithtesting.controller;
 
 import com.example.apiwithtesting.dto.PetEntityDTO;
 import com.example.apiwithtesting.entity.BreedEntity;
+import com.example.apiwithtesting.entity.PetEntity;
 import com.example.apiwithtesting.service.PetService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,7 +21,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
@@ -57,5 +64,34 @@ class PetControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.idPet", is( petEntityDTO.getIdPet() )));
+    }
+
+    @Nested
+    @DisplayName("List Ops - ")
+    public class TestListOperations {
+        @Captor
+        ArgumentCaptor<String> petNameCaptor;
+
+        List<PetEntity> petEntityList;
+
+        @BeforeEach
+        void setup() {
+            List<PetEntityDTO> petEntityDTOList = new ArrayList<>();
+            petEntityDTOList.add(petEntityDTO);
+            petEntityDTOList.add(petEntityDTO);
+            petEntityList = petEntityDTOList.stream()
+                    .map(PetEntityDTO::toEntity).collect(Collectors.toList());
+
+            given(petService.findAll())
+                    .willReturn(petEntityList);
+        }
+
+        @Test
+        @DisplayName("List pets - no args")
+        void testGetAllPets() throws Exception {
+            mockMvc.perform(get("/pet")
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        }
     }
 }
